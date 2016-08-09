6bbb32c2490b103dda83698515b585cb4c036094
Jersey Deng <jersdeng@gmail.com>
2016-08-08 23:48:33 -0700

    return top 10 epicenters and dice coefs

--- find_epicenter.py
+++ find_epicenter.py
@@ -22,42 +22,36 @@ def dicecoef(a, b):
     return round(overlap * 2.0/(len(a_voxels) + len(b_voxels)), 3)
 
 def find_epicenter(wmap_path, w_thr, n_overlap, FC_thr, mask_path='/data/mridata/jbrown/brains/gm_mask/merged_ho_cereb_stn_comb.nii'):
-    """
-    Returns the parcel that is the epicenter for a subject.
+    """Returns the parcel that is the epicenter for a subject.
     
     Parameters
     ----------
     wmap_path : string
         The absolute path to the wmap.
     w_thr : int
-        The lower threshold of the wmap, inclusive.
+        The lower w-score threshold of the wmap, inclusive.
     n_overlap : int
         The minimum number of overlapping voxels between the wmap and a parcel for that parcel to be considered as an epicenter candidate.
     FC_thr : int
-        The lower threshold of the parcel-seeded mean functional connectivity seed maps, inclusive. Appropriate lines should be commented out of the
-        source code depending on whether the threshold is a percentile or an absolute z-score.
+        The lower percentile threshold of the parcel-seeded mean functional connectivity seed maps, inclusive.
     mask_path : string, optional
         The absolute path to the image which will be used to mask the wmap.
         
     Returns
     -------
     out : string
-        A statement about which parcel has the best-fitting seed map to the wmap and the Dice coefficient of the seed map and the wmap OR
-        A statement about the top three parcels with the best-fitting seed maps to the wmap and their Dice coefficients.
-        Appropriate lines should be commented out of the source code depending on which output is desired.
-        
-    Examples
-    --------
-    >>> fe.find_epicenter('/data5/patientNIC/3521/3521_20100114/struc/SPM12_SEG_Full/wmap_123SD_vs_288HC/wmap_2mm.nii', 1.5, 1, 0.5)
-    3521 20100114 vol_158.nii 0.256000
+        The indices of the parcels with the best-fitting seed maps to the wmap and their respective Dice coefficients.
     """
-    # Find parcels that contain at least "n_overlap" voxels of overlap with the "mask"-masked "w_thr"-thresholded wmap and keep their FC seed maps
+    
+    # Mask and threshold wmap
     wmap = nib.load(wmap_path).get_data()
     thr = wmap.ravel() >= w_thr
     mask = nib.load(mask_path).get_data()
     mask = np.ma.make_mask(mask.ravel())
     mask_thr = np.logical_and(mask, thr)
     mask_thr_wmap = np.where(mask_thr == True)[0]
+    
+    # Find parcels that contain at least "n_overlap" voxels of overlap with the masked and thresholded wmap and keep their FC seed maps
     for i in L.keys():
         parcel = L[i].ravel()
         parcel = np.nonzero(parcel)[0]
@@ -70,33 +64,21 @@ def find_epicenter(wmap_path, w_thr, n_overlap, FC_thr, mask_path='/data/mridata
         
     # Threshold each seed map
     for i in L.keys():
-        # uncomment if FC_thr is an absolute cutoff
-        #L[i] = np.nonzero(L[i] >= FC_thr)[0]
-        
-        # uncomment if FC_thr is a percentile cutoff
+        # FC_thr is percentile cutoff
         FC_thr_val = np.percentile(L[i][L[i] != 0], FC_thr)
         L[i] = np.nonzero(L[i] >= FC_thr_val)[0]
 
-    # Get the best-fitting thresholded seed map(s) to the masked & thresholded wmap by taking the thresholded seed map with the greatest Dice coefficient to the wmap
+    # Get the best-fitting thresholded seed maps to the masked & thresholded wmap by taking the thresholded seed maps
+    # with the greatest Dice coefficients to the wmap
     for i in L.keys():
         L[i] = dicecoef(mask_thr_wmap, L[i])
-    
-    L_z = L.copy()
-    for i in L_z:
-        L_z[i] = (L[i] - np.mean(L.values())) / np.std(L.values())
         
+    # Separate L into a list of parcel IDs and a list of Dice coefficients, then sort them
+    parcel_IDs = L.keys()
+    dice_coefficients = L.values()
+    sorted_indices = np.argsort(dice_coefficients)
+    
+    # top 10 epicenters and Dice coefficients
     pid = wmap_path.split("/")[4].split("_")[0]
     date = wmap_path.split("/")[4].split("_")[1]
-    # uncomment if only the top epicenter and its Dice coefficient is wanted
-    print('%s %s vol_%d.nii %f' % (pid, date, max(L, key=L.get), max(L.values())))    
-    
-    # # uncomment if top 3 epicenters and their Dice coefficients are wanted
-    # first = sorted(L, key=L.get)[-1]; second = sorted(L, key=L.get)[-2]; third = sorted(L, key=L.get)[-3]
-    # print('%s %s vol_%d.nii vol_%d.nii vol_%d.nii %f %f %f %f %f %f' % (pid, date, first, second, third, L[first], L[second], L[third], L_z[first], L_z[second], L_z[third]))
-    
-    # # uncomment if a file containing all Dice coefficients is wanted
-    # np.savetxt("/".join(wmap_path.split("/")[:5])+'/struc/epicenters/dice_coefficients.txt', sorted(L_z.values()), fmt='%1.3f')
-    
-    # # addendum code to return top 4 epicenters for Jesse's group map
-    # first = sorted(L, key=L.get)[-1]; second = sorted(L, key=L.get)[-2]; third = sorted(L, key=L.get)[-3]; fourth = sorted(L, key=L.get)[-4]
-    # print('vol_%d.nii vol_%d.nii vol_%d.nii vol_%d.nii %f %f %f %f %f %f %f %f' % (first, second, third, fourth, L[first], L[second], L[third], L[fourth], L_z[first], L_z[second], L_z[third], L_z[fourth]))
+    print '%s %s' % (pid, date), np.array(parcel_IDs)[sorted_indices][-1:-10:-1], np.array(dice_coefficients)[sorted_indices][-1:-10:-1]
