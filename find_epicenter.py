32f94bdb9158b325d3023f7f04be57d1f9251d64
Jersey Deng <jersdeng@gmail.com>
2016-08-18 22:24:40 -0700

    committing changes before switching branches

--- find_epicenter.py
+++ find_epicenter.py
@@ -7,45 +7,60 @@ def threshold_image_using_percentile_threshold(img_data, percentile_threshold_le
 	Parameters
 	----------
 	img_data : ndarray
-		Image in the form of a 1D array of voxel values.
+		Voxel values of an image.
 	threshold_level : int or float
-		Defines cutoff for which any voxels greater than or equal to this value will be kept; otherwise the voxels will be zeroed out.
+		Percentile cutoff for which any voxels greater than or equal to this value will be kept; otherwise the voxels will be zeroed out.
+		
 	Returns
 	-------
-	thresholded_image_indices: ndarray
-		Indices of thresholded image as a 1D array of voxel values.
+	thresholded_image_indices: 1darray
+		Indices of voxels in image that are greater than or equal to the threshold.
 	"""
 	nonzero_img_data = img_data[img_data != 0]
 	img_threshold = np.percentile(nonzero_img_data, percentile_threshold_level)
-	thresholded_image = img_data >= img_threshold
+	thresholded_image = (img_data >= img_threshold)
 	thresholded_image_indices = np.where(thresholded_image.ravel())[0]
 	return thresholded_image_indices
 
 def return_indices_of_masked_thr_voxels(img_path, threshold_level, mask_path='/data/mridata/jbrown/brains/gm_mask/merged_ho_cereb_stn_comb.nii'):
-	"""
+	"""Return indices of voxels in an image that are within a mask and greater than or equal to a threshold.
+	
+	Parameters
+	----------
+	img_path : str
+		Absolute path to an image.
+	threshold_level : int or float
+		Absolute cutoff for which any voxels greater than or equal to this value will be kept; otherwise the voxels will be zeroed out.
+	mask_path : str
+		Absolute path to a mask.
+		
+	Returns
+	-------
+	in_mask_and_above_threshold_voxels_indices : 1darray
+		Indices of voxels in image that are within the mask and greater than or equal to the threshold.
 	"""
 	raw_image = nib.load(img_path).get_data().ravel()
 	mask = nib.load(mask_path).get_data().ravel()
 	in_mask_voxels_boolean = np.ma.make_mask(mask)
-	above_threshold_voxels_boolean = raw_image > threshold_level
+	above_threshold_voxels_boolean = (raw_image >= threshold_level)
 	in_mask_and_above_threshold_voxels_boolean = in_mask_voxels_boolean & above_threshold_voxels_boolean
 	in_mask_and_above_threshold_voxels_indices = np.where(in_mask_and_above_threshold_voxels_boolean)[0]
 	return in_mask_and_above_threshold_voxels_indices
 
-def filter_epicenters_by_parcel_overlap(img_indices, min_overlap):
+def filter_parcels_by_overlap_with_image(img_indices, min_overlap):
 	"""Keep the parcel as an epicenter candidate if it shares at least the specified number of voxels of overlap with the image.
 	
 	Parameters
 	----------
-	img_indices : ndarray
-		Indices in the image.
+	img_indices : 1darray
+		Indices of voxels in the image.
 	min_overlap : int or float
 		Defines cutoff for which a parcel that overlaps the image by any number of voxels greater than or equal to this cutoff will be kept.
 
 	Returns
 	-------
 	epicenter_candidates : list
-		List of candidate epicenter indices.
+		List of candidate epicenters.
 	"""
 	epicenter_candidates = []
 	epicenter_parcel_dict = pickle.load(open('/data/mridata/jdeng/tools/find_epicenter/epicenter_parcel_dict.p', 'r'))
@@ -59,7 +74,19 @@ def filter_epicenters_by_parcel_overlap(img_indices, min_overlap):
 	return epicenter_candidates
 
 def create_epicenter_thr_seedmap_dict(candidates_list, percentile_threshold_level):
-	"""
+	"""Return a dictionary of key-value pairs where keys are epicenters and values are the indices of voxels in the epicenter-seeded functional connectivity map that are greater than or equal to the threshold.
+	
+	Parameters
+	----------
+	candidates_list : list
+		Epicenters.
+	percentile_threshold_level : int or float
+		Percentile cutoff for which any voxels greater than or equal to this value will be kept; otherwise the voxels will be zeroed out.
+		
+	Returns
+	-------
+	epicenter_thr_seedmap_dict : dict
+		Epicenters and the indices of voxels in the epicenter-seeded functional connectivity map that are greater than or equal to the threshold.
 	"""
 	epicenter_seedmap_dict_all = pickle.load(open('/data/mridata/jdeng/tools/find_epicenter/epicenter_seedmap_dict.p', 'r'))
 	epicenter_seedmap_dict = {i: epicenter_seedmap_dict_all[i] for i in candidates_list}
@@ -73,7 +100,7 @@ def dicecoef(a, b):
 
 	Parameters
 	----------
-	a, b : lists or arrays
+	a, b : Lists or arrays.
 	
 	Returns
 	-------
@@ -88,10 +115,23 @@ def dicecoef(a, b):
 		overlap = len(a_voxels & b_voxels)
 		total = len(a_voxels) + len(b_voxels)
 		assert total > 0
-		return round((overlap * 2.0) / total, 3)
+		dc = (overlap * 2.0) / total
+		return round(dc, 3)
 
 def find_epicenter(img_indices, epicenter_thr_seedmap_dict):
-	"""
+	"""Find the best-fitting epicenter(s) for the image.
+	
+	Parameters
+	----------
+	img_indices : 1darray
+		Indices of voxels in the image.
+	epicenter_thr_seedmap_dict : dict
+		Epicenters and the indices of voxels in the epicenter-seeded functional connectivity map that are greater than or equal to the threshold.
+		
+	Returns
+	-------
+	out : str
+		Sorted epicenters and the Dice coefficients of their functional connectivity maps to the image.
 	"""
 	epicenter_dicecoef_dict = {i: dicecoef(img_indices, epicenter_thr_seedmap_dict[i]) for i in epicenter_thr_seedmap_dict.keys()}
 
@@ -103,6 +143,6 @@ def find_epicenter(img_indices, epicenter_thr_seedmap_dict):
 
 if __name__ == '__main__':
 	mask_thr_indices = return_indices_of_masked_thr_voxels('tests/wmap.nii', 2.0)
-	epicenter_candidates = filter_epicenters_by_parcel_overlap(mask_thr_indices, 10)
+	epicenter_candidates = filter_parcels_by_overlap_with_image(mask_thr_indices, 10)
 	epicenter_thr_seedmap_dict = create_epicenter_thr_seedmap_dict(epicenter_candidates, 90)
 	find_epicenter(mask_thr_indices, epicenter_thr_seedmap_dict)
--- tests/test_find_epicenter.py
+++ tests/test_find_epicenter.py
@@ -5,6 +5,8 @@ from numpy.testing.utils import assert_allclose, assert_equal
 
 mask = '/data/mridata/jbrown/brains/gm_mask/merged_ho_cereb_stn_comb.nii'
 
+def test_threshold_image_using_percentile_threshold():
+
 def test_return_indices_of_masked_thr_voxels():
 	img2process = 'wmap.nii'
 	result = return_indices_of_masked_thr_voxels(img2process, 2.0)
