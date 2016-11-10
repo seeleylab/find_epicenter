a27853744fdaeda4f82fa3101826a46c1eeed8b4
Jersey Deng <jersdeng@gmail.com>
2016-11-09 16:09:13 -0800

    add command for making pickled epicenter-seedmap dict with 75 good motion HC mean seedmaps

--- find_epicenter/make_pickled_dicts.py
+++ find_epicenter/make_pickled_dicts.py
@@ -12,3 +12,7 @@ with open('epicenter_seedmap_dict_all.p', 'w') as f:
 epicenter_seedmap_dict_all_v2 = {parcel_index: nib.load('/data/mridata/jdeng/sd_bvftd/100_controls_v2/1ST_vol_%s/spmT_0001.nii' % parcel_index).get_data() for parcel_index in range(1, 274)}
 with open('epicenter_seedmap_dict_all_v2.p', 'w') as f:
   pickle.dump(epicenter_seedmap_dict_all_v2, f)
+
+epicenter_seedmap_dict_all_75hc = {parcel_index: nib.load('/data/mridata/jdeng/sd_bvftd/75_controls/1ST_vol_%s/spmT_0001.nii' % parcel_index).get_data() for parcel_index in range(1, 274)}
+with open('epicenter_seedmap_dict_all_75hc.p', 'w') as f:
+  pickle.dump(epicenter_seedmap_dict_all_75hc, f)
